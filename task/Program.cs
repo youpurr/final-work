bf8348a0faebb64474f8f23964050487875a0a22
unknown <facturs@mail.com>
2023-01-11 19:25:28 +0700

    task111

---
+++ task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] input = new string[5] {"123", "23", "hello", "world", "res"};
+string[] output = new string[input.Length];
+void SecondArray (string[] input, string[] output)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+    if(input[i].Length <= 3)
+        {
+        output[count] = input[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] input)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        Console.Write($"{input[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray (input, output);
+PrintArray(output);
